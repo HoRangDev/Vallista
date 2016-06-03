d51dacfd118ef8853017931aec9de25619fbe7f7
HoRang <kails_@naver.com>
2016-06-03 20:34:44 +0900

    Add Projectile Movement
    /

--- Assets/Resources/Scripts/Monster.cs
+++ Assets/Resources/Scripts/Monster.cs
@@ -3,5 +3,44 @@ using System.Collections;
 
 public class Monster : MonoBehaviour
 {
+    [SerializeField]
+    private int _Health;
 
+    [SerializeField]
+    private float _Speed;
+
+    void OnTriggetEnter2D(Collider2D Other)
+    {
+        switch(Other.tag)
+        {
+            case "Projectile":
+                Projectile TargetProjectile = Other.GetComponent(typeof(Projectile)) as Projectile;
+                if(TargetProjectile != null)
+                {
+                    _Health -= TargetProjectile.Damage;
+                    TargetProjectile.Destroy();
+
+                    if(_Health <= 0)
+                    {
+                        Dead();
+                    }
+                    else
+                    {
+                        Attacked();
+                    }
+                }
+                break;
+        }
+    }
+
+    void Attacked()
+    {
+        //@TODO: Add Attacked Animation
+    }
+
+    void Dead()
+    {
+        //@TODO: Add Dead Animation and Destroy Process
+
+    }
 }
--- Assets/Resources/Scripts/Projectile.cs
+++ Assets/Resources/Scripts/Projectile.cs
@@ -3,14 +3,78 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField]
-    private Mesh _Mesh;
+    private SpriteRenderer _SpriteRenderer;
     private Transform _Transform;
-    private Renderer _Renderer;
+
+    [SerializeField]
+    private int _StartDamage;
+    [SerializeField]
+    private int _MaxDamage;
+    private int _CurrentDamage;
+    public int Damage { get { return _CurrentDamage; } }
+
+    [SerializeField]
+    private float _Speed;
+    private Vector2 _DirectionVector;
+    public Vector2 DirectionVector { get { return _DirectionVector; } set { _DirectionVector = value; } }
+
+    [SerializeField]
+    private float _DestroyTime;
+    private float _ElasedTime;
 
     void Awake()
     {
         _Transform = transform;
-        _Renderer = GetComponent(typeof(Renderer)) as Renderer;
+        _SpriteRenderer = GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+
+        _CurrentDamage = _StartDamage;
+    }
+
+    public void SetToMove()
+    {
+        StartCoroutine("ProjectileMovement");
+    }
+
+    IEnumerator ProjectileMovement()
+    {
+        Vector2 MoveVector = _DirectionVector * _Speed * Timer.Instance.DeltaTime;
+        _Transform.position = new Vector2(_Transform.position.x + MoveVector.x, _Transform.position.y + MoveVector.y);
+        yield return null;
+    }
+
+    IEnumerator ProjectileDestroy()
+    {
+        _ElasedTime += Timer.Instance.DeltaTime;
+        if(_ElasedTime >= _DestroyTime)
+        {
+            StopAllCoroutines();
+            Destroy(gameObject);
+        }
+        else
+        {
+            float FadeOutRatio = 1.0f - Mathf.Clamp01((_ElasedTime / _DestroyTime));
+            Color SpriteColor = _SpriteRenderer.color;
+            SpriteColor.a = FadeOutRatio;
+            _SpriteRenderer.color = SpriteColor;
+        }
+
+        yield return null;
+    }
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        switch(Other.tag)
+        {
+            case "Wall":
+                _DirectionVector.x *= -1.0f;
+                _CurrentDamage = Mathf.Clamp(_CurrentDamage * 2, _StartDamage, _MaxDamage);
+                break;
+        }
+    }
+
+    public void Destroy()
+    {
+        StopCoroutine("ProjectileMovement");
+        StartCoroutine("ProjectileDestroy");
     }
 }
