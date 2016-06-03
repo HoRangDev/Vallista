89e788afe8b9586dc1093c9bb763545f5444a1a1
Junho Park <wnsghekd2006@naver.com>
2016-06-03 17:18:40 +0900

    Add LogoScene

---
+++ Assets/Resource/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ParkJunHo
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        public float Second = 0f;
+        public int Level = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            SceneComponent.GetInstance().ScheduleLoadScene(Second, Level);
+        }
+    }
+}
