963609ece0408bd337c232738044c709b7a4c2ba
Junho Park <wnsghekd2006@naver.com>
2016-06-03 19:47:55 +0900

    Added Back Button Observer

---
+++ Assets/Resources/Scripts/BackText.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace ParkJunHo
+{
+    public class BackText : MonoBehaviour
+    {
+        private Image image = null;
+        private float speed = 0.02f;
+
+        // Use this for initialization
+        void Start()
+        {
+            image = GetComponent<Image>();
+        }
+
+        public void ShowText()
+        {
+            Color cor = image.color;
+            cor.a = 1;
+            image.color = cor;
+
+            StartCoroutine(TextShowed());
+        }
+
+        private IEnumerator TextShowed()
+        {
+            yield return new WaitForSeconds(0.7f);
+
+            while(true)
+            {
+                Color cor = image.color;
+                cor.a -= speed * 62.5f * Time.smoothDeltaTime;
+                image.color = cor;
+
+                if(cor.a <= 0)
+                {
+                    break;
+                }
+
+                yield return null;
+            }
+        }
+    }
+
+}
